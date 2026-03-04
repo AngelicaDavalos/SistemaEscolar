2bddfdf6963bf326a4a2ab964da99962a1f04b72
godeckc <20253rd@utez.edu.mx>
2026-03-04 13:38:46 -0600

    Es un trabajo con Dana y Jessica

--- Alumno.java
+++ Alumno.java
@@ -7,12 +7,14 @@ public class Alumno {
     private double calif3;
 
 
+
     public Alumno(String nombre, String matricula){
         this.nombre = nombre;
         this.matricula = matricula;
     }
  
-    
+
+
 
 
     public void setCalificaciones(double calif1, double calif2, double calif3){
@@ -21,10 +23,12 @@ public class Alumno {
         this.calif3 = calif3;
     }
 
+
     public double calcularPromedio(){
         return (calif1 + calif2 + calif3) / 3;
     }
 
+    
     public void mostrarAlumno(){
         System.out.println("El alumno es: " +this.nombre);
         System.out.println("La matricula es: " +this.matricula);
