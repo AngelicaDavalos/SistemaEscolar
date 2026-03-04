e05353ae78f1477eb36fe9111d4b185267d54491
AngelicaDavalos <20253rd007@utez.edu.mx>
2026-03-04 13:20:47 -0600

    Creamos una funcion para agregar alumnos , pero solo acepta a 5 y manda mesaje de error cuando esta lleno el arreglo if

--- Escuela.java
+++ Escuela.java
@@ -8,5 +8,30 @@ public class Escuela {
 
     private int contador = 0;
 
+    // Agregar alumno
+    public void agregarAlumno(Alumno alumno) {
+
+        if (contador == 0) {
+            alumno1 = alumno;
+        } else if (contador == 1) {
+            alumno2 = alumno;
+        } else if (contador == 2) {
+            alumno3 = alumno;
+        } else if (contador == 3) {
+            alumno4 = alumno;
+        } else if (contador == 4) {
+            alumno5 = alumno;
+        } else {
+            System.out.println("No se pueden agregar más alumnos.");
+            return;
+        }
+
+        contador++;
+        System.out.println("Alumno agregado correctamente.");
+    }
+
     
 }
+
+    
+
