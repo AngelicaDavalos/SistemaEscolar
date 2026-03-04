0042a7f5b162169f36a300c257398844180b9869
AngelicaDavalos <20253rd007@utez.edu.mx>
2026-03-04 13:06:52 -0600

    Creamos las variables para guardar datos de 5 alumnos

--- Escuela.java
+++ Escuela.java
@@ -1,3 +1,12 @@
 public class Escuela {
     
+    private Alumno alumno1;
+    private Alumno alumno2;
+    private Alumno alumno3;
+    private Alumno alumno4;
+    private Alumno alumno5;
+
+    private int contador = 0;
+
+    
 }
